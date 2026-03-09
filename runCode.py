7f9fa33e1ebe20cbae3cfa11b43326084a948cee
Victor Nwosu <uzyatforums@gmail.com>
2026-03-09 12:50:23 +0100

    Added runCode file

---
+++ runCode.py
@@ -0,0 +1,12 @@
+print("Hello, welcome to the adder!")
+
+def add(a, b):
+    return a + b
+
+number1 = 10
+number2 = 20
+
+print("Adding number " + str(number1) + " and " + str(number2))
+sum = add(number1, number1)
+
+print("The sum is: " + str(sum))
